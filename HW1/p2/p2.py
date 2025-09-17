0058d85d3899c589b7423334d1dd74ed8eef7e48
Kaijin <92873112+TWCkaijin@users.noreply.github.com>
2025-09-17 11:49:48 +0800

    update HW2 and set entry in toml file

---
+++ HW1/p2/p2.py
@@ -0,0 +1,20 @@
+from ..apis.data_api import get_data
+import numpy as np 
+from matplotlib import pyplot as plt 
+import pandas as pd
+
+plt.rcParams['font.sans-serif'] = ['Arial Unicode MS', 'SimHei', 'DejaVu Sans']
+plt.rcParams['axes.unicode_minus'] = False
+
+def main():
+    entry_data, exit_data, date_data = get_data(['202501','202502','202503'])
+    station = np.random.choice(entry_data.keys(),(1,), replace=False)
+    
+    plt.plot(entry_data.get_full_list()[station[0]], label='Raw Data', linewidth=1.5, color="red")
+    for d in range(3,9):
+        mov_avg = entry_data.get_full_list()[station[0]].rolling(window=d).mean()
+        plt.plot(mov_avg, label=f'{d}-Day Moving Average')
+    plt.xlabel('Date')
+    plt.ylabel(f'Entry of {station[0]}')
+    plt.legend()
+    plt.show()
